b57d368d0dc2dca6c326372c6f15db365804f61f
Gavin Quirk <gavinquirk@gmail.com>
2019-10-28 12:50:12 -0700

    generator obj, dom el vars, generate listener

--- main.js
+++ main.js
@@ -1,5 +1,47 @@
-// Generator functions
+// DOM Elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
 
+// Generator Object
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+// Generate event listen
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value; // Convert from num to string
+  const hasUpper = uppercaseEl.checked;
+  const hasLower = lowercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+// Generate Password
+function generatePassword(lower, upper, number, symbol, length) {
+  // Init password var
+  // Filter out unchecked types
+  // Loop over length, call generator func for each type
+  // Add final password var and return
+}
+
+// Random Generator Functions
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
